672dbba59be9085cc1783438add3d08fc7774349
Ethan Gray <74423016+KafLad@users.noreply.github.com>
2022-03-06 18:25:22 -0800

    SBT v0.1
    Beginning of SmartBotTesting branch

--- locGetter.py
+++ locGetter.py
@@ -1,6 +1,16 @@
 import pyautogui as pag
 import time
 
-while True:
-    time.sleep(0.5)
-    print(pag.position())    
+def constLocating():
+    while True:
+        time.sleep(0.5)
+        print(pag.position())
+
+def locTest():
+    out = pag.position()
+    print(out[0])
+    print(out[1])
+    print(out)
+
+if __name__ == "__main__":
+    locTest()
